07c397be165c96b309a494f225b0ddddf7b9c36d
yerim <pppp5097@naver.com>
2022-01-20 02:29:38 +0900

    React - Search Form

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,4 +1,15 @@
+import { formatRelativeDate } from "./js/helpers.js";
 import store from "./js/Store.js";
+
+const TabType = {
+  KEYWORD: "KEYWORD",
+  HISTORY: "HISTORY",
+};
+
+const TabLabel = {
+  [TabType.KEYWORD]: "추천 검색어",
+  [TabType.HISTORY]: "최근 검색어",
+};
 
 class App extends React.Component {
   constructor() {
@@ -8,7 +19,20 @@ class App extends React.Component {
       searchKeyword: "",
       searchResult: [],
       submitted: false,
+      selectedTab: TabType.KEYWORD,
+      keywordList: [],
+      historyList: [],
     };
+  }
+
+  componentDidMount() {
+    const keywordList = store.getKeywordList();
+    const historyList = store.getHistoryList();
+
+    this.setState({
+      keywordList,
+      historyList,
+    });
   }
 
   handleSubmit(event) {
@@ -18,8 +42,12 @@ class App extends React.Component {
 
   search(searchKeyword) {
     const searchResult = store.search(searchKeyword);
+    const historyList = store.getHistoryList();
+
     this.setState({
+      searchKeyword,
       searchResult,
+      historyList,
       submitted: true,
     });
   }
@@ -40,6 +68,14 @@ class App extends React.Component {
     }
 
     this.setState({ searchKeyword });
+  }
+
+  handleClickRemoveHistory(event, keyword) {
+    event.stopPropagation();
+
+    store.removeHistory(keyword);
+    const historyList = store.getHistoryList();
+    this.setState({ historyList });
   }
 
   render() {
@@ -75,6 +111,50 @@ class App extends React.Component {
         <div className="empty-box">검색 결과가 없습니다</div>
       );
 
+    const keywordList = (
+      <ul className="list">
+        {this.state.keywordList.map(({ id, keyword }, index) => (
+          <li key={id} onClick={() => this.search(keyword)}>
+            <span className="number">{index + 1}</span>
+            <span>{keyword}</span>
+          </li>
+        ))}
+      </ul>
+    );
+
+    const historyList = (
+      <ul className="list">
+        {this.state.historyList.map(({ id, keyword, date }) => (
+          <li key={id} onClick={() => this.search(keyword)}>
+            <span>{keyword}</span>
+            <span className="date">{formatRelativeDate(date)}</span>
+            <button
+              className="btn-remove"
+              onClick={(event) => this.handleClickRemoveHistory(event, keyword)}
+            />
+          </li>
+        ))}
+      </ul>
+    );
+
+    const tabs = (
+      <>
+        <ul className="tabs">
+          {Object.values(TabType).map((tabType) => (
+            <li
+              key={tabType}
+              className={this.state.selectedTab === tabType ? "active" : ""}
+              onClick={() => this.setState({ selectedTab: tabType })}
+            >
+              {TabLabel[tabType]}
+            </li>
+          ))}
+        </ul>
+        {this.state.selectedTab === TabType.KEYWORD && keywordList}
+        {this.state.selectedTab === TabType.HISTORY && historyList}
+      </>
+    );
+
     return (
       <>
         <header>
@@ -82,7 +162,9 @@ class App extends React.Component {
         </header>
         <div className="container">
           {searchForm}
-          <div className="content">{this.state.submitted && searchResult}</div>
+          <div className="content">
+            {this.state.submitted ? searchResult : tabs}
+          </div>
         </div>
       </>
     );
